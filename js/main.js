c6799d9447f8efef349c4178aace8f6d2f9d8a0e
Chandru <65664480+B-chandru@users.noreply.github.com>
2021-01-10 10:18:01 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,19 +4,18 @@ function encrypt() {
     var list = document.createElement("li");
     var button = document.createElement("button");
     var input = document.createElement("input");
-    var a= document.getElementById("text").value;
-    if( a==="") {
+    var text= document.getElementById("text").value;
+    if( text==="") {
       alert("please enter any message!")
     } else {
      
-      var b64=btoa(a);
+      var b64=btoa(text);
        var unicode=atob(b64) ;
       list.innerHTML = `${b64} <span>(${unicode})</span>`;
       
        button.innerHTML = "delete";
        button.style = "margin:15px;color: white;border: none;outline:none;text-transform: uppercase;padding: 10px;background:#448AFF;";
   
-      //  console.log(unicode);
 
       list.style =
         "color:white;font-family: Trebuchet MS, Lucida Sans Unicode, Lucida Grande,Lucida Sans, Arial, sans-serif;font-size: larger;";
@@ -24,11 +23,6 @@ function encrypt() {
       number.appendChild(list);
       list.appendChild(button);
 
-      // button.addEventListener("mouseenter",() => {
-      //   button.style =  "margin:15px;color: #448AFF;border: none;outline:none;text-transform: uppercase;padding: 10px; background-color:white;";
-        
-      // });
-     
   
       button.addEventListener("click", function () {
         list.remove();
